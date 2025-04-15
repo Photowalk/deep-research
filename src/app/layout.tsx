31cffb133d47b3640d60d898acf9694c6646cb95
Photowalk <3141902+Photowalk@users.noreply.github.com>
2025-04-15 09:14:54 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/layout.tsx
@@ -0,0 +1,54 @@
+import type { Metadata, Viewport } from "next";
+import Script from "next/script";
+import ThemeProvider from "@/components/Provider/Theme";
+import I18Provider from "@/components/Provider/I18n";
+import { Toaster } from "@/components/ui/sonner";
+
+import "./globals.css";
+
+const HEAD_SCRIPTS = process.env.HEAD_SCRIPTS as string;
+
+export const metadata: Metadata = {
+  title: "Deep Research",
+  description: "Deep Rssearch with Google Gemini Models",
+  icons: {
+    icon: {
+      type: "image/svg+xml",
+      url: "./logo.svg",
+    },
+  },
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1.0,
+  minimumScale: 1.0,
+  maximumScale: 1.0,
+  viewportFit: "cover",
+  userScalable: false,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" dir="auto" suppressHydrationWarning>
+      <head>
+        {HEAD_SCRIPTS ? <Script id="headscript">{HEAD_SCRIPTS}</Script> : null}
+      </head>
+      <body className="antialiased">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <I18Provider>{children}</I18Provider>
+        </ThemeProvider>
+        <Toaster richColors toastOptions={{ duration: 3000 }} />
+      </body>
+    </html>
+  );
+}
